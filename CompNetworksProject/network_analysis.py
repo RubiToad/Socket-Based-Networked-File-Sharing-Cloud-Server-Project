8619f25cc42fefe6dc9965895c667095ce846501
RubiToad <tylermondalto@gmail.com>
2024-11-19 16:46:54 -0500

    Up to date network analysis error

--- CompNetworksProject/network_analysis.py
+++ CompNetworksProject/network_analysis.py
@@ -18,6 +18,7 @@ def get_time_offset():
         print(e)
     return resp.offset
 
+
 def current_client_time(ntp_offset):
     current_date = datetime.now().date()
     initial_time = datetime.now().time()
@@ -26,3 +27,16 @@ def current_client_time(ntp_offset):
     return f'{new_datetime}'
     #print(get_time_offset())
 
+# makes a calculation using the bytes recieved and time offset to get the upload speed in MB/s
+def get_upload_speed():
+    from server import bytes_recieved, bytes_sent, time_difference
+    bytes_MB = bytes_recieved / (1024 * 1024)  # converts the bytes into MB
+    speed = bytes_MB / time_difference # calculates the upload speed in MB/s
+    return speed
+
+# makes a calculation using the bytes sent and time offset to get the download speed in MB/s
+def get_download_speed():
+    from server import bytes_recieved, bytes_sent, time_difference
+    dlBytes_MB = bytes_sent / (1024 * 1024) # converts the bytes into MB
+    download_speed = dlBytes_MB / time_difference # calculates the download speed in MB/s
+    return download_speed
